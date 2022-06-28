3abd293e55455c079960bd3909724e8e131160c7
carpincoc <c-andres-1211@hotmail.com>
2022-06-28 10:23:20 -0500

    Ajustes varios Se ajusta StepperImages para que funcione ocn React18 Se arregla error de react-map-gl

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import MapGl from 'react-map-gl';
 import { MAP_ACCESS_TOKEN } from '../../config/entrypoint';
+import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const Map = (props) => {
     return (
--- src/components/stepper/StepperImages.js
+++ src/components/stepper/StepperImages.js
@@ -5,10 +5,7 @@ import MobileStepper from '@mui/material/MobileStepper';
 import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 
 const StepperImages = ({ images }) => {
@@ -30,30 +27,17 @@ const StepperImages = ({ images }) => {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AutoPlaySwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents
-            >
-                {images.map((step, index) => (
-                    <div key={`img${index}`}>
-                        {Math.abs(activeStep - index) <= 2 ? (
-                            <Box
-                                component="img"
-                                sx={{
-                                    height: 255,
-                                    display: 'block',
-                                    overflow: 'hidden',
-                                    width: '100%',
-                                }}
-                                src={step.path}
-                                alt={step.label}
-                            />
-                        ) : null}
-                    </div>
-                ))}
-            </AutoPlaySwipeableViews>
+            <Box
+                component="img"
+                sx={{
+                    height: 255,
+                    display: 'block',
+                    overflow: 'hidden',
+                    width: '100%',
+                }}
+                src={images[activeStep].path}
+                alt={images[activeStep].label}
+            />
             <MobileStepper
                 steps={maxSteps}
                 position="static"
